5c639ab0a2a82450568a3c1d546b437610081543
JennaB17 <>
2021-04-28 15:45:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+first_number,second_number = int(input("What is the number?")),int(input("What is your second number?"))
+# asks the user for the first number or use two lines
+print(first_number, second_number)
